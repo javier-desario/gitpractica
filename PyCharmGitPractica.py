4b0bd0a2940143ce00dfe2bb76c1faaa698bb591
Javier <desarioj@hotmail.com>
2021-03-05 15:19:31 -0300

    added pie chart y mse en modelo grafico

--- PyCharmGitPractica.py
+++ PyCharmGitPractica.py
@@ -3,11 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 sns.set()
 
-def linreg_plotter(m=2, n=100, e=250):
+
+def linreg_model(m=2, n=100, e=250):
     """Plotea modelo de regresion lineal con parametros de pendiente m, ruido e y size n"""
+
+    # seed para la obtencion de random numbers
+    np.random.seed(42)
 
     # Genero datos
     x = np.linspace(0, 1000, n)
@@ -20,15 +25,53 @@ def linreg_plotter(m=2, n=100, e=250):
     model = LinearRegression()
     model.fit(X, y)
     y_pred = model.predict(X)
+    mse = mean_squared_error(y, y_pred)
+
+    # df para analisis
+    df = pd.DataFrame({"x": x, "y": y.reshape(n, ), "y_pred": y_pred.reshape(n, )})
+    df['Residuos +'] = 0
+    df['Residuos -'] = 0
+    df.loc[(df['y'] <= df['y_pred']), 'Residuos +'] = 1
+    df.loc[(df['y'] > df['y_pred']), 'Residuos -'] = 1
+
+    rpos = sum(df['Residuos +'])
+    rneg = sum(df['Residuos -'])
+
+    data = [rpos, rneg]
+
+    return x, y, y_pred, data, mse
+
+
+def linreg_plotter(m=2, n=100, e=250):
+    """plotea datos entregados por linreg_model"""
+
+    # Cargo datos
+    x, y, y_pred, data, mse = linreg_model(m, n, e)
 
     # Ploteado
-    _ = plt.plot(X, y, color='orange')
-    _ = plt.plot(X, y_pred, color='blue')
+    _ = plt.plot(x, y, color='orange')
+    _ = plt.plot(x, y_pred, color='blue')
 
-    _ = plt.xlabel("x")
-    _ = plt.ylabel("y")
+    _ = plt.xlabel("Valores de x")
+    _ = plt.ylabel("Valores de y")
     _ = plt.legend(['Valores reales', 'Valores modelados'], loc='lower right')
+    _ = plt.title('Modelo de Regresion lineal por Least Squares Method')
+    _ = plt.annotate('mse = {:.2f}'.format(mse), xy=[10, 2000])
+    plt.show()
+
+
+def pie_plot(m=2, n=100, e=250):
+    """Pie plot en base a los datos generados en linreg_plotter"""
+
+    # Cargo datos
+    x, y, y_pred, data, mse = linreg_model(m, n, e)
+
+    # ploteo
+    _ = plt.pie(data, autopct='%1.1f%%')
+    _ = plt.legend(['Residuos +', 'Residuos -'], loc='lower right')
+    _ = plt.title('Residuos generados por Least Squares Method')
     plt.show()
 
 
 linreg_plotter()
+pie_plot()
